2fd608b5aca674725de8b0e54749521ea4e54079
Roberta Ferreira Aguilar <roberta.aguilar@kobel.com.br>
2024-09-26 19:30:58 -0300

    ProductFiles

--- Server/Server/Data/ApplicationDbContext.cs
+++ Server/Server/Data/ApplicationDbContext.cs
@@ -10,8 +10,9 @@ public class ApplicationDbContext : DbContext
 
 
     public DbSet<Pizza> Pizzas { get; set; } = null!;
-    public DbSet<Customer> Customer { get; set; } = null!;
-    public DbSet<Address> Address { get; set; } = null!;
+    public DbSet<CustomerModel> Customers { get; set; } = null!;
+    public DbSet<AddressModel> Address { get; set; } = null!;
 
+    public DbSet<ProductModel> Products { get; set; }
 
 }
---
+++ Server/Server/Data/Dtos/Product/CreateProductDto.cs
@@ -0,0 +1,26 @@
+﻿using Server.Data.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace Server.Data.Dtos.Product
+{
+    public class CreateProductDto
+    {
+
+        [Required(ErrorMessage = "O campo Nome é obrigatório")]
+        public string Name { get; set; }
+
+        public string? Description { get; set; }
+
+        [Required(ErrorMessage = "O campo Tipo é obrigatório")]
+
+        public ProductEnum.Category Category { get; set; }
+
+        [Required(ErrorMessage = "O campo Quantidade é obrigatório")]
+
+        public int Quantity { get; set; }
+
+        [Required(ErrorMessage = "O campo Valor é obrigatório")]
+
+        public decimal Price { get; set; }
+    }
+}
